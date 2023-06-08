7729f0c44749742ed4379252c732d57b7f1a4eea
Geree <smithzero94@gmail.com>
2023-06-08 20:21:43 +0200

    1. exercise done. BankAccount class defined, private fields created, getter, setter methods defined and called in main.

--- Main.java
+++ Main.java
@@ -5,5 +5,23 @@ public class Main {
 
         Car defaultCar = new Car();
         System.out.println(defaultCar.getModel()); // Output: Unknown
+
+        BankAccount firstAccount = new BankAccount();
+        firstAccount.setAccountHolder("Deborah Granger");
+        firstAccount.setAccountNumber("000001");
+        firstAccount.setBalance(1500000);
+
+        BankAccount secondAccount = new BankAccount();
+        secondAccount.setAccountHolder("Lucios Wayne");
+        secondAccount.setAccountNumber("000002");
+        secondAccount.setBalance(200000);
+
+        System.out.println("Name: " + firstAccount.getAccountHolder() +
+                ", account number: " + firstAccount.getAccountNumber() +
+                ", balance: " + firstAccount.getBalance() + " HUF");
+
+        System.out.println("Name: " + secondAccount.getAccountHolder() +
+                ", account number: " + secondAccount.getAccountNumber() +
+                ", balance: " + secondAccount.getBalance() + " HUF");
     }
 }
